88d92f89e4130ad854d1fce76903b41b2d62d42a
OlaOst <olaa81@gmail.com>
2012-06-11 20:53:59 +0200

    Getting rack ids and rack info

--- main.d
+++ main.d
@@ -1,7 +1,22 @@
+import std.conv;
+import std.net.curl;
 import std.stdio;
 
 
 void main(string args[])
 {
-  writeln("hello world");
+  string racks = cast(string)get("http://smartbikeportal.clearchannel.no/public/mobapp/maq.asmx/getRacks");
+  
+  string rack = getRack(1);
+  
+  writeln(rack);
+}
+
+
+
+string getRack(int rackId)
+{
+  string raw = cast(string)get("http://smartbikeportal.clearchannel.no/public/mobapp/maq.asmx/getRack?id=" ~ to!string(rackId));
+  
+  return raw;
 }
